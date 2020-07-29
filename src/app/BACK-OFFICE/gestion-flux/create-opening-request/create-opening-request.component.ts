ef7f9ed76fb41c580621a1d0c38ae6a3e13e5396
Nidhal AROURI <nidhal.arouri@newaccess.ch>
2020-07-29 13:52:17 +0100

    Add some todos for future bug fix

--- src/app/BACK-OFFICE/gestion-flux/create-opening-request/create-opening-request.component.ts
+++ src/app/BACK-OFFICE/gestion-flux/create-opening-request/create-opening-request.component.ts
@@ -1,61 +1,47 @@
-import { Component,EventEmitter, OnInit, Output } from '@angular/core';
-import {Router} from '@angular/router';
-import {Observable} from 'rxjs';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Flux } from 'src/app/entities/Flux';
-import {FluxService} from'../../../services/flux/flux.service';
-
-
-
+import { FluxService } from '../../../services/flux/flux.service';
 
 @Component({
   selector: 'app-create-opening-request',
   templateUrl: './create-opening-request.component.html',
   styleUrls: ['./create-opening-request.component.css'],
-
 })
-export class CreateOpeningRequestComponent  {
-
+export class CreateOpeningRequestComponent implements OnInit {
   @Output() closeAll = new EventEmitter<boolean>();
   flux: Flux;
   submitted = false;
-  id:number;
-  fluxs:any = [];
+  id: number;
+  fluxs: any = [];
   fluxsList: Observable<Flux>;
-  constructor(private fluxService:FluxService, private router: Router) {
-    this.flux= new Flux();
-   }
+  constructor(private fluxService: FluxService, private router: Router) {
+    this.flux = new Flux();
+  }
 
- 
   ngOnInit() {
     this.submitted = false;
-    //this.flux= new Flux();
+    this.flux = new Flux();
   }
 
   save() {
     console.log(this.flux);
-    this.fluxService.createFlux(this.flux)
-      .subscribe(
-        data => console.log("msg:",data),
-        error1 => console.log(error1)
-      );
-    this.flux= new Flux();
+    this.fluxService.createFlux(this.flux).subscribe(
+      (data) => console.log('msg:', data),
+      (error1) => console.log(error1)
+    );
+    this.flux = new Flux();
     this.goToList();
-  } 
+  }
 
-
-goToList() {
-  this.fluxService.getFluxs();
-  this.router.navigate(['admin/FluxList']);
+  goToList() {
+    this.fluxService.getFluxs();
+    this.router.navigate(['admin/FluxList']);
+  }
+  closeThis() {
+    this.submitted = true;
+    this.save();
+    this.closeAll.emit(true);
+  }
 }
-closeThis() {
-  this.submitted = true;
-  this.save();
-  this.closeAll.emit(true);
- }
-  
-
-}
-
-
-
-
